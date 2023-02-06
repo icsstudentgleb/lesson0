ad9d34b7c241bce0e2c54d659438e03267bf106e
Gleb Lavin <gleb.lavin@ics-it.ru>
2023-02-06 20:08:45 +0700

    Fim semicolon

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -3,7 +3,7 @@ create table dbo.SKU(
     Code as 's' + cast(ID as varchar),
     Name varchar(255),
     constraint PK_SKU primary key (ID),
-    constraint UK_SKU_Code unique(Code))
+    constraint UK_SKU_Code unique(Code));
 
 create table dbo.Basket(
     ID int identity not null, 
@@ -14,10 +14,10 @@ create table dbo.Basket(
     DiscountValue int,
     constraint PK_Basket primary key (ID),
     constraint FK_Basket_ID_SKU_SKU foreign key(ID_SKU) references dbo.SKU(ID),
-    constraint CK_Basket_Quantity_Value check (Quantity > 0 and Value > 0))
+    constraint CK_Basket_Quantity_Value check (Quantity > 0 and Value > 0));
     
 create table dbo.Family(
     ID int identity not null,
     SurName varchar(255),
     BudgetValue int,
-    constraint PK_Family primary key (ID))
+    constraint PK_Family primary key (ID));
