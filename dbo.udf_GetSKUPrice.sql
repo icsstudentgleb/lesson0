7f41aa572a1a0b625e096630376c1ce5ddce2654
Gleb Lavin <gleb.lavin@ics-it.ru>
2023-02-06 19:52:19 +0700

    INT-14210 30. Функции

---
+++ dbo.udf_GetSKUPrice.sql
@@ -0,0 +1,15 @@
+create function dbo.udf_GetSKUPrice(
+    @ID_SKU as int
+)
+returns decimal(18,2)
+as
+    begin
+        declare @result decimal(18,2);
+    
+        select @result = cast(sum(Value) as decimal) / sum(Quantity)
+        from dbo.Basket
+        where ID_SKU = 1;
+    
+        return @result
+    end;
+    
